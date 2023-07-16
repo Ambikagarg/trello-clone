b0da5255a020bfe6b183a6df84afcd611007bdbc
Ambikagarg <64024928+Ambikagarg@users.noreply.github.com>
2023-07-16 16:39:32 +0530

    Delete BoardBar.js

--- BoardBar.js
+++
@@ -1,9 +0,0 @@
-import './BoardBar.scss';
-const BoardBar = () => {
-    return (
-        <>
-        <nav className='navbar-board'> Board bar</nav>
-        </>
-    )
-}
-export default BoardBar;
